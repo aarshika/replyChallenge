915c6cca305b354b9a95113d7fc9061f6d39a796
pssingh21 <peeyushmansingh@gmail.com>
2020-03-12 18:06:58 +0100

    updated

--- replyChallenge.cpp
+++ replyChallenge.cpp
@@ -1,44 +1,71 @@
+#include <algorithm>
 #include <iostream>
 #include <iterator>
 #include <set>
-#include <algorithm>
 using namespace std;
 
 class Employee {
-    public:
-
-        string company;
-        int bonus;
+public:
+    string company;
+    int bonus;
 };
 
-class Developer: Employee {
-    public:
+class Developer : Employee {
+public:
+    string *skills;
+    int skillCount;
 
-        string skills[];
-};
-
-class Manager: Employee{};
-
-int WP(Developer di, Developer dj){
-    for(int i = 1; i < sizeof(di.skills)/sizeof(di.skills[0]); i++){
-        cout << di.skills[i] << endl;
+    Developer(string company, int bonus, int skillCount, string *skills) {
+        this->bonus = bonus;
+        this->company = company;
+        this->skillCount = skillCount;
+        this->skills = new string[skillCount];
+        for (int i = 0; i < skillCount; i++) {
+            this->skills[i] = skills[i];
+            cout << this->skills[i];
+        }
     }
+};
+
+class Manager : Employee {};
+
+int WP(Developer di, Developer dj) {
+    set<string> si;
+    for(int i = 0; i < di.skillCount; i++){
+        si.insert(di.skills[i]);
+    }
+
+    set<string> sj;
+    for(int i = 0; i < dj.skillCount; i++){
+        sj.insert(dj.skills[i]);
+    }
+
+    set<int> intersect;
+    set_intersection(si.begin(),si.end(),sj.begin(),sj.end(), back_inserter(intersect));
+                  
+    cout << intersect.size();
 }
 
-int main()
-{
+int main() {
+    string *s1 = new string[2];
+    s1[0] = "sad";
+    s1[1] = "asd";
+    Developer d1("opn", 7, 2, s1);
+    Developer d2("opn", 7, 2, s1);
+    WP(d1, d2);
+
     // string a = "abcdk57";
     // string b = "abcdk57";
     // string c;
 
-    set_intersection(
-        a.begin(), a.end(),
-        b.begin(), b.end(),
-        back_inserter(c));
+    // set_intersection(
+    //     a.begin(), a.end(),
+    //     b.begin(), b.end(),
+    //     back_inserter(c));
 
-        if(!c.compare(a) && !c.compare(b)){
-            cout << "same";
-        }
+    //     if(!c.compare(a) && !c.compare(b)){
+    //         cout << "same";
+    //     }
 
-    cout << c << '\n';
+    // cout << c << '\n';
 }
