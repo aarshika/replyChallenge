6ae9f7e0f81cb750386b329895e0f3a860a4a198
pssingh21 <peeyushmansingh@gmail.com>
2020-03-12 17:27:13 +0100

    sad

--- replyChallenge.cpp
+++ replyChallenge.cpp
@@ -1,14 +1,44 @@
 #include <iostream>
+#include <iterator>
+#include <set>
+#include <algorithm>
 using namespace std;
 
 class Employee {
-    string company;
-    int bonus;
+    public:
+
+        string company;
+        int bonus;
 };
 
 class Developer: Employee {
-    string Skills;
+    public:
+
+        string skills[];
 };
 
 class Manager: Employee{};
 
+int WP(Developer di, Developer dj){
+    for(int i = 1; i < sizeof(di.skills)/sizeof(di.skills[0]); i++){
+        cout << di.skills[i] << endl;
+    }
+}
+
+int main()
+{
+    // string a = "abcdk57";
+    // string b = "abcdk57";
+    // string c;
+
+    set_intersection(
+        a.begin(), a.end(),
+        b.begin(), b.end(),
+        back_inserter(c));
+
+        if(!c.compare(a) && !c.compare(b)){
+            cout << "same";
+        }
+
+    cout << c << '\n';
+}
